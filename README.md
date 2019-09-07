8214e0f410ee5bcd0601a78b0fdd2116dd31da29
Rafal Gorczynski <rafal.gorczynski@gmail.com>
2019-09-07 12:53:45 +0200

    Progress, README status

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+[![Build Status](https://travis-ci.com/rawfg/chwaeth.svg?branch=master)](https://travis-ci.com/rawfg/chwaeth)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import SpotifyAvatar from './SpotifyAvatar';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -26,15 +25,12 @@ export default function NavigationBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar classes={{ colorPrimary: 'black', colorDefault: 'red', colorSecondary: 'red' }} position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <SpotifyAvatar />
         </Toolbar>
       </AppBar>
     </div>
